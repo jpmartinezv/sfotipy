41673779d3a025b37e1c8c403ac0be70a2a5c98e
Jason Martinez <jason1994.8@gmail.com>
2014-08-23 12:32:42 -0500

    Add userprofiles module: signup and signin

--- tracks/views.py
+++ tracks/views.py
@@ -1,10 +1,25 @@
+import json
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
 from .models import Track
 
 def track_view(request, title):
-    
+
     track = get_object_or_404(Track, title=title)
 
-    return render(request, 'track.html', {'track' : track})
+    bio = track.artist.biography;
+
+    data = {
+            'title' : track.title,
+            'order' : track.order,
+            'album' : track.album.title,
+            'artist' : {
+                'name' : track.artist.first_name,    
+                'bio' : bio,    
+                }
+            }
+
+    json_data = json.dumps(data)
+
+    return HttpResponse(json_data, content_type = 'application/json')
